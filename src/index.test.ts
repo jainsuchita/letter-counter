67f5df29d01019b20218255edc9f908b72d28ff5
jainsuchi21 <jainsuchi21@gmail.com>
2019-09-16 21:04:59 +0530

    Update the solution

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,34 @@
 import { letterCounter } from 'index';
 
-describe('WHEN counting letters', () => {
+describe('should not return undefined', () => {
   it('true should be true', () => {
-    const result = letterCounter();
-    expect(result).toBe(true);
+    const result = letterCounter('hello');
+    expect(result).not.toBe(undefined);
+  });
+});
+
+describe('should return empty object', () => {
+  it('true should be true', () => {
+    const result = letterCounter('');
+    expect(Object.keys(result).length).toBe(0);
+  });
+});
+
+describe('should return objects of length 4', () => {
+  it('true should be true', () => {
+    const result = letterCounter('hello');
+    expect(Object.keys(result).length).toBe(4);
+  });
+});
+
+describe('count of all letters should be same as length of the string', () => {
+  it('true should be true', () => {
+    const input = 'blahblah';
+    const result = letterCounter(input);
+    const len = Object.keys(result).reduce((total, letter) => {
+      total = total + result[letter];
+      return total;
+    }, 0);
+    expect(len).toBe(input.split('').length);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-export const letterCounter = () => {
-  console.log('letterCounter');
-  return null;
+// export const letterCounter = () => {
+//   console.log('letterCounter');
+//   return null;
+// };
+
+// interface ILetterCounter {
+//   letter: string;
+//   occurences: number;
+// }
+
+export const letterCounter = (input) => {
+  let arr = input.split('');
+  let newArr = arr.reduce((obj, letter) => {
+    if (!obj[letter]) {
+      obj[letter] = 1;
+    } else {
+      obj[letter]++;
+    }
+    return obj;
+  }, {});
+
+  console.log(newArr);
+  return newArr;
 };
